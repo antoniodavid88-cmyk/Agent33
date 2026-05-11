4a3bae8e17ea2ef11f587edb7b5c6c2859771e8d
antoniodavid88-cmyk <antoniodavid88@gmail.com>
2026-05-11 22:02:37 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>AI Content Machine</h1>
+      <a href="https://buy.stripe.com/28E28k1Ul87H3kY8SeefC09">
+        <button>Accès 99€</button>
+      </a>
+    </div>
+  );
+}
